763b0cf9a8f9d5ad3184d433a9e5bc38137f6f75
unknown <michaela.macejovska@yahoo.com>
2020-05-06 14:34:59 +0200

    add Bank of Simba

---
+++ BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<BankAccount> bankAccounts = BankAccount.InitializeList();
+
+        [HttpGet("show")]
+        public IActionResult Index()
+        {
+            return View(bankAccounts);
+        }
+        [HttpPost]
+        public IActionResult RaiseBalance()
+        {
+            foreach (var item in bankAccounts)
+            {
+                if(item.IsKing)
+                {
+                    item.Balance += 100;
+                }
+                else
+                {
+                    item.Balance += 10;
+                }
+            }
+            return View("index",bankAccounts);
+        }
+        [HttpPost]
+        public IActionResult AddNewAccount(string name, decimal balance, string animalType, bool isKing, bool isGood)
+        {
+            var bankAccounts = new List<BankAccount>();
+            bankAccounts.Add(new BankAccount(name, balance, animalType, isKing, isGood));
+            return View("index", bankAccounts);
+        }
+
+        [HttpGet("showText")]
+        public IActionResult ShowText()
+        {
+            ViewBag.message = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";            
+            return View();
+        }
+    }
+}
---
+++ BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public string AnimalType { get; set; }
+        public bool IsKing { get; set; }
+        public bool IsGood { get;set; }
+        public BankAccount(string name, decimal balance, string animalType, bool isKing, bool isGood)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+            IsKing = isKing;
+            IsGood = isGood;
+        }
+        public BankAccount()
+        {
+
+        }
+        public static List<BankAccount> InitializeList()
+        {
+            var bankAccounts = new List<BankAccount>()
+            {
+                new BankAccount("Simba",2000,"lion", true, true),
+                new BankAccount("Scar",3000,"lion", true, false),
+                new BankAccount("Mustafa", 5000, "lion", true, true),
+                new BankAccount("Timon",800,"meerkat", false, true),
+                new BankAccount("Pumbaa",1200,"pig", false, true),
+                new BankAccount("Kamari",400,"hyena", false, false)
+            };
+            return bankAccounts;
+        }
+    }
+}
